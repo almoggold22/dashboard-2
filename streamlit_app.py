1729978762846d2d452b47301403a8222dcdd7fd
almoggold22 <156449451+almoggold22@users.noreply.github.com>
2024-03-16 04:13:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,13 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
 st.set_page_config(
     page_title="Feedbacks Dashboard",
-    layout='wide')
+    layout='wide',
+    initial_sidebar_state='collapsed')
 st.sidebar.success("Select a Page above")
 
 #Establishing a Google Sheets connection
@@ -19,7 +22,7 @@ url = 'https://docs.google.com/spreadsheets/d/1kSeOTYs4XPxA2Rqb3Jz5tezwjllUmbRfB
 page_col_1, page_col_2, page_col_3, page_col_4, page_col_5, page_col_6, page_col_7, page_col_8, page_col_9, page_col_10 = st.columns(10)
 with page_col_1:
     on = st.toggle(":blue[**Dislike**]")
-if on:
+if on: #on = Dislike
     with page_col_10:
         st.link_button("'Dislike' Sheet",'https://docs.google.com/spreadsheets/d/1kSeOTYs4XPxA2Rqb3Jz5tezwjllUmbRfBO1csTILHvE/edit#gid=1763036692')
     st.title(":grey[Feedbacks Dashboard] - :blue[Dislike]")
@@ -38,7 +41,6 @@ if on:
     col1, col2, col3 = st.columns((3))
     with col2:
         model_type = st.selectbox("**Choose Model**", models_dates_col_left)
-
 
     #Dates Range
     if model_type:
@@ -75,7 +77,7 @@ if on:
                 return name_of_buttons_col_left
 
     #columns for pie charts
-    col_model1, col_model2, col_model3, col_model4, col_model5 = st.columns((5))
+    col_model1, col_model2, col_model3, col_model4, col_model5, col_model6, col_model7, col_model8, col_model9, col_model10, col_model11, col_model12, col_model13, col_model14, col_model15, col_model16, col_model17, col_model18 = st.columns((18))
     #---2---Pie-Charts--
     #'translation in' columns - left side
     existing_data_left = conn.read(spreadsheet=url, worksheet='1763036692', usecols=list(range(9, 75)), ttl=5)
@@ -88,23 +90,22 @@ if on:
     size_right = right_range.groupby(buttons_col_of_each_model_left[index_finder_left(models_dates_col_left)]).size()
     pie_chart_right = px.pie(right_range, values=size_right, names=size_right.index, height = 500, width= 450, hole=.4)
     df_right_dis = pd.DataFrame(size_right)
-
-    with col_model1:
+    df_right_dis.style.bar()
+    with col_model2:
         st.plotly_chart(pie_chart_left)
-    with col_model4:
+    with col_model13:
         st.plotly_chart(pie_chart_right)
 
-    dfcol1, dfcol2, dfcol3, dfcol4, dfcol5 = st.columns((5))
-    with dfcol1:
-        dfcol_left = st.dataframe(df_left_dis, width=400)
-    with dfcol4:
-        dfcol_right = st.dataframe(df_right_dis, width=400)
+    #dfcol1, dfcol2, dfcol3, dfcol4, dfcol5 = st.columns((5))
+    #with dfcol1:
+    #    dfcol_left = st.dataframe(df_left_dis, width=400)
+    #with dfcol4:
+    #    dfcol_right = st.dataframe(df_right_dis, width=400)
 
     st.divider()
 
 
-
-else:
+else: #on = All
     #Compare Page:
     #Display Title and Despcription
     with page_col_10:
@@ -166,7 +167,7 @@ else:
                 return name_of_buttons_col_left
 
     #columns for pie charts
-    col_model1, col_model2, col_model3, col_model4, col_model5 = st.columns((5))
+    col_model1, col_model2, col_model3, col_model4, col_model5, col_model6, col_model7, col_model8, col_model9, col_model10, col_model11, col_model12, col_model13, col_model14, col_model15, col_model16, col_model17, col_model18 = st.columns((18))
     #---2---Pie-Charts--
     #'translation in' columns - left side
     existing_data_left = conn.read(spreadsheet=url, worksheet='1664217404', usecols=list(range(9, 75)), ttl=5)
@@ -181,15 +182,50 @@ else:
     pie_chart_right = px.pie(right_range, values=size_right, names=size_right.index, height = 500, width= 450, hole=.4)
     df_right_all = pd.DataFrame(size_right)
 
-    with col_model1:
+    with col_model2:
         st.plotly_chart(pie_chart_left)
-    with col_model4:
+    with col_model13:
         st.plotly_chart(pie_chart_right)
 
-    dfcolall1, dfcolall2, dfcolall3, dfcolall4, dfcolall5 = st.columns((5))
-    with dfcolall1:
-        dfcol_left = st.dataframe(df_left_all, width=340)
-    with dfcolall4:
-        dfcol_right = st.dataframe(df_right_all, width=340)
+    #dfcolall1, dfcolall2, dfcolall3, dfcolall4, dfcolall5 = st.columns((5))
+    #with dfcolall1:
+    #    dfcol_left = st.dataframe(df_left_all, width=340)
+    #with dfcolall4:
+    #    dfcol_right = st.dataframe(df_right_all, width=340)
+
 
     st.divider()
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#new plot
+empty_list_for_table_data = ['']
+table_data = [empty_list_for_table_data]
+#df = pd.DataFrame(dict(name = [size_left.index], num = [size_left]))
+fig_1 = ff.create_table(table_data)
+
+trace1 = go.Bar(x=size_left.index, y=size_left, xaxis='x2', yaxis='y2',
+                marker=dict(color='#0099ff'),
+                name='Left',
+                showlegend=True,
+                text=size_left)
+trace2 = go.Bar(x=size_right.index, y=size_right, xaxis='x2', yaxis='y2',
+                marker=dict(color='#404040'),
+                name='Right',
+                showlegend=True,
+                text=size_right)
+
+fig_1.add_traces([trace1, trace2])
+
+fig_1.layout.margin.update({'t':75, 'l':50})
+fig_1.layout.update({'title': 'Models Comparison'})
+fig_1.layout.update({'height':500})
+fig_1.layout.update(({'width':1100}))
+
+
+fig_col1, fig_col2, fig_col3, fig_col4, fig_col5, fig_col6, fig_col7, fig_col8, fig_col9 = st.columns((9))
+
+with fig_col2:
+    fig_col2_show = st.write(fig_1)
+
+st.divider()
